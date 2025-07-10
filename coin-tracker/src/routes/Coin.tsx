1ea24883828c8d4d2278ca6d5c5b2b231d6b85fb
Zbell0 <but05051@gmail.com>
2025-07-10 21:40:38 +1000

    1.5 data type

--- coin-tracker/src/routes/Coin.tsx
+++ coin-tracker/src/routes/Coin.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import styled from "styled-components";
 
@@ -18,13 +18,63 @@ const Header = styled.header`
   align-items: center;
 `;
 
+interface IInfoData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  logo: string;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  first_data_at: string;
+  last_data_at: string;
+}
+
+interface IPriceData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: object;
+}
+
 function Coin() {
   const { coinId } = useParams();
   const [loading, setLoading] = useState(true);
+  const [info, setInfo] = useState<IInfoData>({});
+  const [priceInfo, setPriceInfo] = useState<IPriceData>({});
+  useEffect(() => {
+    (async () => {
+      const infoData = await (
+        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+      ).json();
+      console.log(infoData);
+      const priceData = await (
+        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+      ).json();
+      console.log(priceData);
+      setInfo(infoData);
+      setPriceInfo(priceData);
+    })();
+  }, []);
   const {
     state: { name },
   } = useLocation();
-  console.log(name);
 
   return (
     <Container>
